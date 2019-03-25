1e6d16772542da5f4c22cf988bb0f6873bf24a5f
Ildar Akhmetgaleev <akhilman@gmail.com>
2019-03-25 15:32:10 +0700

    Add test for enums.

---
+++ tests/cpp/enums.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE Table with enums
+#include "enums_flatboobs.h"
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+using namespace flatboobs::schema::test;
+
+BOOST_AUTO_TEST_CASE(test_default_values) {
+  TestEnum table{};
+
+  BOOST_TEST(table.test_enum() == TestEnum::default_values::test_enum());
+  BOOST_TEST(table.test_flag() == TestEnum::default_values::test_flag());
+}
+
+BOOST_AUTO_TEST_CASE(test_flags) {
+  TestEnum table{TestEnumFlag::Bar | TestEnumFlag::Buz, TestEnumEnum::Foo};
+
+  std::cout << table << std::endl;
+}
